a9ffd8e509490106f3fce9ca3ed1c8cfd47c84b8
perchespierri <perchespierri@gmail.com>
2022-03-18 08:08:59 -0300

    Create test template

---
+++ src/tests/testTemplate.test.js
@@ -0,0 +1,16 @@
+// imports:
+// import { render, screen } from "@testing-library/react";
+// import userEvent from "@testing-library/user-event";
+
+// mocking data:
+// jest.mock("../../data/productsData", () => ({
+//   productsData: [{ id: "1", name: "Mjolnir", description: "Martelo do Thor" }],
+// }));
+
+// describe("What you are testing", () => {
+//   it("should do something specific", () => {
+//     render(<Component/>);
+//     const variable = ...
+//     expect(variable).to...
+//   })
+// })
